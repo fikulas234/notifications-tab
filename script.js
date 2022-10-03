785c79ebddf19dcd5ce1caab252d8d4003be3323
fikulas234 <trbojevicfilip96@gmail.com>
2022-10-03 14:57:07 +0200

    added some change for small random functionality

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ const notificationsContainer = document.querySelectorAll('.notification');
 const notificationsNumber = document.querySelector('.notif-num');
 const redDotNotification = document.querySelectorAll('.red-dot');
 
+// event listeners
 clearNotifBtn.addEventListener('click', clearnotifications);
 
+// functions
+
+// removing all unread notifications
 function clearnotifications() {
   notificationsContainer.forEach(function (notif) {
     notif.classList.remove('unread');
@@ -15,3 +19,21 @@ function clearnotifications() {
 
   notificationsNumber.textContent = 0;
 }
+
+// getting random number
+const rndNum = Math.floor(Math.random() * 7);
+
+// taking random number and setting the same number of unread notifications
+function randomizeNotifications() {
+  const unreadNotif = notificationsContainer;
+  const redDot = redDotNotification;
+  notificationsNumber.textContent = rndNum;
+  let i = 0;
+  while (i < rndNum) {
+    unreadNotif.item(i).classList.add('unread');
+    redDot.item(i).classList.add('show');
+    i++;
+  }
+}
+
+randomizeNotifications();
